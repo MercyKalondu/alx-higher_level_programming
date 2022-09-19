396fdceb715591c2405e067d47bd8f2bfb708957
MercyKalondu <mercykalondu62@gmail.com>
2022-09-19 12:48:12 +0000

    N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""This module contains an algorithm that resolves the N-Queen puzzle
-using backtracking
+"""This module contains an algorithm that resolves the N-Queen puzzle using backtracking
 """
 
 
