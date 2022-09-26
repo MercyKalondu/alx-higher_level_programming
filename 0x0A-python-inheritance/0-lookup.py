fa02dbd974b170ab9a4605ebb529408ff79900b1
MercyKalondu <mercykalondu62@gmail.com>
2022-09-26 11:36:46 +0000

    available attributes and methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """Function that returns the list of available attributes and methods of an object
+
+    Args:
+        obj: instance of class
+
+    Returns:
+        Lists of attributes
+    """
+
+    return dir(obj)
