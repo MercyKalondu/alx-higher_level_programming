6900a35f2a616e80117e8faccf58a0f8a0ed1076
MercyKalondu <mercykalondu62@gmail.com>
2022-09-27 13:17:21 +0000

    append to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """
-Module that contains a function that returns the number of lines of a text file
+Contains the function "wrtie_file"
 """
 
 
-def number_of_lines(filename=""):
-    """returns the number of lines of a text file"""
-    with open(filename, 'r', encoding='utf8') as f:
-        i = 0
-        for i in f:
-            i += 1
-        return i
+def write_file(filename="", text=""):
+    """returns the number of chars written to "filename" from "text" """
+    with open(filename, 'w', encoding='utf=8') as f:
+        return f.write(text)
