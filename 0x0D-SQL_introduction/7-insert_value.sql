626bbbbe9383e14483ea8e6c34a1804fac454b5e
MercyKalondu <mercykalondu62@gmail.com>
2022-11-01 05:54:05 -0700

    insert value

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- Insert a new row in a tavle
---Query to insert a new row in the tab le first_table
+--Query to insert a new row in the table first_table
 INSERT INTO first_table (id, name) VALUES (89, "Best School")
