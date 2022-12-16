23fd6a766f3c60948c2ddff8231ee51c6bdd5c18
MercyKalondu <mercykalondu62@gmail.com>
2022-12-16 11:12:35 -0800

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-Python script that takes in a URL, sends a request and displays
+"""Python script that takes in a URL, sends a request and displays
 the value of the X-Request-Id variable found in the header
-(handling HTTP errors)
-"""
+(handling HTTP errors)"""
+
 from urllib import request, error
 import sys
-
 
 if __name__ == "__main__":
     try:
