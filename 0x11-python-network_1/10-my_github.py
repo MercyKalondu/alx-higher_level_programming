58c5ce12f2e68080f3bdba49c0af97820fd05ba4
MercyKalondu <mercykalondu62@gmail.com>
2022-12-16 11:02:42 -0800

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""
-Python script that takes GitHub credentials
-and uses the GitHub API to display the user id
-"""
+
+"""Python script that takes GitHub credentials and uses the GitHub API to display the user id"""
+
 from requests import get, auth
 import sys
-
 
 if __name__ == "__main__":
     url = 'https://api.github.com/user'
