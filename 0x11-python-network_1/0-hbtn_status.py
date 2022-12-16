51a3caf985cad942e314b846a766b794a776a6a0
MercyKalondu <mercykalondu62@gmail.com>
2022-12-16 10:06:38 -0800

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
 
+import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
