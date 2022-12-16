c156a8556c409c9b989f548ff4e59609ec6db78b
MercyKalondu <mercykalondu62@gmail.com>
2022-12-16 11:07:43 -0800

    post email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""
-Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email,
-and displays the body of the response
-"""
+
+"""Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email, and displays the body of the response"""
+
 import requests
 import sys
-
 
 if __name__ == "__main__":
     r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
